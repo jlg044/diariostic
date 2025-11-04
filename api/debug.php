11e7fd9f7da57c24cb1ecac3a7f5a899929e507b
José Luis López García <90789168+jlg044@users.noreply.github.com>
2025-11-04 19:23:05 +0100

    Create debug.php

---
+++ api/debug.php
@@ -0,0 +1,8 @@
+<?php
+echo "<pre>";
+echo "HOST = " . getenv('MYSQL_HOST') . "\n";
+echo "USER = " . getenv('MYSQL_USER') . "\n";
+echo "PASS = " . getenv('MYSQL_PASSWORD') . "\n";
+echo "DB   = " . getenv('MYSQL_DATABASE') . "\n";
+echo "</pre>";
+?>
